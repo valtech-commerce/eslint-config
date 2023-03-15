7caee48b99c1a79d0e15bdad0f8eb889fc10bf15
Jean-Baptiste Landry <jb.landry@valtech.com>
2023-03-15 08:39:19 -0400

    Remove pkg-dir

--- src/get-config.js
+++ src/get-config.js
@@ -5,15 +5,12 @@ const path = require("node:path");
 const { fsSync } = require("@valtech-commerce/fs");
 const deepMerge = require("deepmerge");
 const prettierConfig = require("eslint-config-prettier");
-const pkgDir = require("pkg-dir");
 
 module.exports = (...paths) => {
 	const shareableConfig = paths.reduce((config, filePath) => {
 		const filePaths = filePath.split("/");
 		const filename = filePaths.pop();
-		const current = fsSync.readYaml(
-			path.join(pkgDir.sync(__dirname), "shareable-config", ...filePaths, `${filename}.yaml`)
-		);
+		const current = fsSync.readYaml(path.join(__dirname, "..", "shareable-config", ...filePaths, `${filename}.yaml`));
 
 		return deepMerge.all([config, current], {
 			arrayMerge: (destination, source) => source,
